61ddb2df7f281eb8672088a4eb503b538b5efac3
Håkon Schia <haakosc@stud.ntnu.no>
2018-09-29 12:20:15 +0200

    Change main

--- main.go
+++ main.go
@@ -1,29 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-
-	if port == "" {
-		log.Fatal("$PORT must be set")
+	// Get port from environment
+	port, ok := os.LookupEnv("PORT")
+	if !ok {
+		port = "8080"
 	}
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
-
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	log.Print("Add handler to root path")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Recived %s from %s for %s", r.Method, r.RemoteAddr, r.URL)
+		fmt.Fprint(w, "Hello world")
 	})
 
-	router.Run(":" + port)
+	log.Printf("Setting up server to listen at port %s", port)
+
+	// Run the server
+	// This will block the current thread
+	err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+
+	// We will only get to this statement if the server unexpectedly crashes
+	log.Fatalf("Server error: %s", err)
 }
