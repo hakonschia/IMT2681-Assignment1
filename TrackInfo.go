ee44866da4ac90721c9f928b2b9ce668c5570cd0
Håkon Schia <haakosc@stud.ntnu.no>
2018-09-29 22:33:10 +0200

    Added TrackInfo struct

---
+++ TrackInfo.go
@@ -0,0 +1,11 @@
+package main
+
+import "time"
+
+type trackInfo struct {
+	HDate       time.Time `json:"H_date"`
+	Pilot       string    `json:"pilot"`
+	Glider      string    `json:"glider"`
+	GliderID    string    `json:"glider_id"`
+	TrackLength float64   `json:"track_length"`
+}
