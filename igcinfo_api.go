69263e2e4f3645ea5a7c7399b160982831601740
Håkon Schia <haakosc@stud.ntnu.no>
2018-10-08 18:59:47 +0200

    Moved structs to one file

--- igcinfo_api.go
+++ igcinfo_api.go
@@ -25,6 +25,45 @@ func init() {
 	nextID = 1 // Start at 1 to avoid potential conversions from string (which returns 0 if not an int)
 }
 
+// Glider contains information about a glider, the pilot and track length
+type Glider struct {
+	HDate       string  `json:"H_date"`
+	Pilot       string  `json:"pilot"`
+	Glider      string  `json:"glider"`
+	GliderID    int     `json:"glider_id"`
+	TrackLength float32 `json:"track_length"`
+}
+
+// APIInfo contains basic information about the API
+type APIInfo struct {
+	Uptime  string `json:"uptime"` // TODO: Convert to string and match the ISO 8601 format
+	Info    string `json:"info"`
+	Version string `json:"version"`
+}
+
+// TrackInfo contains basic information about a track
+type TrackInfo struct {
+	HDate       time.Time `json:"H_date"`
+	Pilot       string    `json:"pilot"`
+	Glider      string    `json:"glider"`
+	GliderID    string    `json:"glider_id"`
+	TrackLength float64   `json:"track_length"`
+}
+
+// FormatISO8601 formats time.Duration to a string according to the ISO8601 standard
+func FormatISO8601(t time.Duration) string {
+	seconds := int64(t.Seconds()) % 60 // These functions return the total time for each field (e.g 200 seconds)
+	minutes := int64(t.Minutes()) % 60 // Using modulo we get the correct values for each field
+	hours := int64(t.Hours()) % 24
+
+	totalHours := int64(t.Hours())
+	days := (totalHours / 24) % 30 // Doesnt really work since it's not 30 days in each month
+	months := (totalHours / (24 * 30)) % 12
+	years := totalHours / (24 * 30 * 12)
+
+	return fmt.Sprint("P", years, "Y", months, "M", days, "DT", hours, "H", minutes, "M", seconds, "S")
+}
+
 // RemoveEmpty removes empty strings from an array
 func RemoveEmpty(arr []string) []string {
 	var newArr []string
