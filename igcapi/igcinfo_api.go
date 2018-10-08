c36dace12e62bd9e223789fe30c99b944faa47c9
Håkon Schia <haakosc@stud.ntnu.no>
2018-10-08 19:48:14 +0200

    Removed duplicate struct

--- igcapi/igcinfo_api.go
+++ igcapi/igcinfo_api.go
@@ -26,17 +26,6 @@ func init() {
 }
 
 /*
-Glider contains information about a glider, the pilot and track length
-*/
-type Glider struct {
-	HDate       string  `json:"H_date"`
-	Pilot       string  `json:"pilot"`
-	Glider      string  `json:"glider"`
-	GliderID    int     `json:"glider_id"`
-	TrackLength float32 `json:"track_length"`
-}
-
-/*
 APIInfo contains basic information about the API
 */
 type APIInfo struct {
